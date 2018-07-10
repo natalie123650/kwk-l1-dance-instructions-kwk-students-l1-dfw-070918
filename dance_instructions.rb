bfc732b973e8a9a242b74acc89b31508ea7f57d7
Natalie Emanuel <natalieazana@gmail.com>
2018-07-10 16:03:34 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,28 @@
 
 # Code your methods below
+
+def gangnam_style
+  starting_stance
+  base_footwork
+  skip-step
+end
+
+def starting_stance
+  puts "Assume the proper stance"
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end
+
+def skip-step
+  def skip_step_right
+    puts "Lower left foot to the ground
+    puts "Bounce left foot back up slightly, kicking it a few inches back"
+    puts "Left, Right, Left and Left"
+  end 
+
+gangnam_style
